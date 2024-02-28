eaa099547d9bfe720c13dfce60a3110d2e4af57d
ANKUSH KUMAR <77243849+Kankush18@users.noreply.github.com>
2024-02-28 17:25:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
-function activateSearch() {
-    var searchDiv = document.getElementById('searchComponent');
-    searchDiv.classList.add('active');
-    var inputField = searchDiv.querySelector('.input');
-    inputField.focus();
-  }
+function changeSearchBar(){
+	let searchBox=document.querySelector('.search');
+	searchBox.classList.add('active');
+	searchBox.focus();
+	
+}
